a802a2d8d1b63ee013ba9d52eaa5ffc62790e84f
PRASHANT\prash <prashant.reddy@kps.com>
2025-08-22 15:59:39 +0100

    Initial commmit

---
+++ GitHubProj01/src/test/java/reddy/psr/GitHubProj01/AppTest.java
@@ -0,0 +1,19 @@
+package reddy.psr.GitHubProj01;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
